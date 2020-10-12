9f2aef223d0cab58eab810b85f37e2cfb1957603
weijianjie <weijianjie@bytedance.com>
2020-10-12 18:41:47 +0800

    Scan Convert Line --barycenteric interpolation(much faster

--- sRenderer/src/main.cpp
+++ sRenderer/src/main.cpp
@@ -9,6 +9,8 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 
+const Vec3f light_dir = Vec3f(0, 0, -1);
+
 Model* model = NULL;
 const int width = 1000;
 const int height = 1000;
@@ -57,7 +59,11 @@ void Line(Vec2i t0, Vec2i t1, TGAImage& image, TGAColor color) {
 
 
 //  Scan Convert Line:
+//#define L2L_SCAN 
+#define POINT_IN_TRIANGLE
 //      Method1: Line-by-line Scan
+
+#ifdef L2L_SCANE
 void Triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
     // sort by y-coordinates;
     if (t0.y > t1.y) std::swap(t0, t1);
@@ -96,10 +102,71 @@ void Triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
     }
 
 }
+#endif // L2L_SCANE
 
 //      Method2: Point In Triangle
-/*  TODO
-void Triange(){}
+///*  TODO
+using Line2i = Vec2i;
+using Triangle3i = Vec2i[3];
+
+//template <int T>
+//using Tx = Vec2i[T];
+
+
+int CrossProduct(Line2i a, Line2i b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+bool Inside(Vec2i p, Triangle3i tri) {
+    //Line2i PA = Line2i(p.x - tri[0].x, p.y - tri[0].y);
+    Line2i  A = tri[1] - tri[2],  B = tri[0] - tri[1],  C = tri[2] - tri[0];
+    Line2i PA = p      - tri[2], PB = p      - tri[1], PC = p      - tri[0];
+
+    
+    int d0 = CrossProduct(PA, A);
+    int d1 = CrossProduct(PB, B);
+    int d2 = CrossProduct(PC, C);
+
+    if (d0 >= 0 && d1 >= 0 && d2 >= 0) return true;
+    if (d0 <= 0 && d1 <= 0 && d2 <= 0) return true;
+
+    return false;
+}
+
+#ifdef POINT_IN_TRIANGLE
+void Triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color){
+    // sort by y-coordinates; (ASC
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t1.y > t2.y) std::swap(t1, t2);
+    if (t0.y > t1.y) std::swap(t0, t1);
+ 
+
+    int boundX = std::min(t0.x, std::min(t1.x, t2.x));
+    int boundWith = std::max(t0.x, std::max(t1.x, t2.x)) - boundX;
+
+    int boundY = t0.y;
+    int bountHeight = t2.y - t0.y;
+
+    // Draw grids
+    //for (int x = boundX; x <= boundX + boundWith; x+=5) {
+    //    for (int y = boundY; y <= boundY + bountHeight; y+=5) {
+    //        image.set(x, y, white);
+    //    }
+    //}
+
+    for (int x = boundX; x <= boundX + boundWith; x++) {
+        for (int y = boundY; y <= boundY + bountHeight; y++) {
+            Vec2i p = Vec2i(x, y);
+
+            if (Inside(p, Triangle3i{ t0, t1, t2})) {
+                image.set(p.x, p.y, color);
+            }
+
+        }
+    }
+}
+#endif // POINT_IN_TRIANGLE
+
 //*/
 
 int main(int argc, char** argv) {
@@ -119,9 +186,10 @@ int main(int argc, char** argv) {
      
      //Line(10, 15, 13, 40, image, red);
 
-    model = new Model("assets/obj/jwj.obj");
+    model = new Model("assets/obj/african_head.obj");
 
     TGAImage image(width, height, TGAImage::RGB);
+
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
 
@@ -137,17 +205,24 @@ int main(int argc, char** argv) {
         //        Line(x0, y0, x1, y1, image, white);
         //    //}
         //}
-
-        Vec2i vertices[3];
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
         for (int j = 0; j < 3; j++) {
             Vec3f v = model->vert(face[j]); 
             //std::cout << v.x << " " << v.y << " " << v.z << std::endl;
             int vx = (v.x + 1.0f) * width / 2.0f;
-            int vy = (v.y + 1.0f) * height / 2.0f - height / 2.0f;
-            vertices[j] = Vec2i(vx, vy);
+            int vy = (v.y + 1.0f) * height / 2.0f;//-height / 2.0f;
+            screen_coords[j] = Vec2i(vx, vy);
+            world_coords[j] = v;
         }
-        Triangle(vertices[0], vertices[1], vertices[2], image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 
+        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);        //   fuck xor!
+        n.normalize();
+
+        float indensity = n * light_dir;
+        if (indensity > 0) {
+            Triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(indensity * 255, indensity * 255, indensity * 255, 255));
+        }
     }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
